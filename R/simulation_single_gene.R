21cf8a495dcc90dd0a1c7d29be7b4cb3a67cd44b
Jiayu Su <jiayu_su@outlook.com>
2021-04-19 00:58:28 -0400

    Add sim options for individual fixed effect

--- R/simulation_single_gene.R
+++ R/simulation_single_gene.R
@@ -48,23 +48,37 @@ simulateReadCounts.s <- function(n_i, mu, phi, prob_as, prob_alt, theta) {
 #' @param maf Minor allele frequency of the test snp
 #' @param prob_ref Probability of the test snp on the ref allele of the target
 #' gene if the test snp is heterogeneous
+#' @param origin A vector specifying sample origin. If \code{NULL} then no two
+#' samples come from a same individual.
 #'
 #' @export
-simulateGenotype.s2s <- function(n_i, maf, prob_ref) {
+simulateGenotype.s2s <- function(n_i, maf, prob_ref, origin = NULL) {
   maf <- ifelse(maf <= 0.5, maf, 1 - maf)
 
-  # simulate two loci
-  h1 <- rbinom(n = n_i, size = 1, prob = maf)
-  h2 <- rbinom(n = n_i, size = 1, prob = maf)
-  # G: n_i by 1, genotype matrix
+  if (is.null(origin)) {
+    # all samples come from different individuals
+    origin <- 1:n_i
+  }
+
+  # simulate two loci for each individual
+  n_indiv <- length(unique(origin))
+  stopifnot(n_indiv > 1)
+
+  h1 <- rbinom(n = n_indiv, size = 1, prob = maf)
+  h2 <- rbinom(n = n_indiv, size = 1, prob = maf)
+  # G: n_indiv by 1, genotype matrix
   G <- h1 + h2
 
-  # P: n_i by 1, phasing matrix
+  # P: n_indiv by 1, phasing matrix
   P <- ifelse(G == 1, 1, 0)
   is_cis <- (runif(n_i) <= prob_ref) * 2 - 1
   P <- P * is_cis
 
-  return(list(Genotype = G, Phasing = P))
+  # calculate G and P for each sample
+  G_tilta <- G[origin]
+  P_tilta <- P[origin]
+
+  return(list(Genotype = G, Phasing = P, origin = origin))
 }
 
 
@@ -88,16 +102,29 @@ softmax <- function(x) {
 #' @param p_error A vector of phasing error rate of the gene pair in each
 #' sample. If \code{length(p_error) == 1} then all samples share the same
 #' phasing error rate
+#' @param origin.effect A vector specifying the effect of sample origin with
+#' length \code{n_indiv} (the number of samples). If \code{NULL} then no effect
+#' will be added.
 #'
 #' @export
-simulateCisEffect.s2s <- function(n_i, maf, prob_ref, phi, prob_as, theta, baseline, r, p_error = NULL) {
+simulateCisEffect.s2s <- function(n_i, maf, prob_ref, phi, prob_as,
+                                  theta, baseline, r, p_error = NULL,
+                                  origin = NULL, origin.effect = NULL) {
 
   # simulate genotype
-  meta <- simulateGenotype.s2s(n_i = n_i, maf = maf, prob_ref = prob_ref)
+  meta <- simulateGenotype.s2s(
+    n_i = n_i, maf = maf, prob_ref = prob_ref, origin = origin
+  )
 
   # simulate genetic effects on total read counts
   log_mu <- baseline + ifelse(meta$Genotype == 1, log(1 + exp(r)) - log(2), 0) +
     ifelse(meta$Genotype == 2, r, 0)
+
+  # simulate confounding factors on total read counts
+  if (!is.null(origin.effect)) {
+    log_mu <- log_mu + origin.effect[meta$origin]
+  }
+
 
   # simulate genetic effects on allelic imbalance
   logit_prob_alt <- meta$Phasing * r
@@ -133,6 +160,9 @@ simulateCisEffect.s2s <- function(n_i, maf, prob_ref, phi, prob_as, theta, basel
       baseline = baseline,
       r = r
     ),
+    conf_pars = list(
+      origin.effect = origin.effect
+    ),
     data = list(
       I = n_i,
       P = meta$Phasing,
@@ -142,7 +172,8 @@ simulateCisEffect.s2s <- function(n_i, maf, prob_ref, phi, prob_as, theta, basel
       log1p_T = log1p(Y$TotalReadCounts),
       A_ref = Y$RefCounts,
       A_alt = Y$AltCounts,
-      Is_ase_het = Y$RefCounts * Y$AltCounts != 0
+      Is_ase_het = Y$RefCounts * Y$AltCounts != 0,
+      Ori = meta$origin
     )
   )
 

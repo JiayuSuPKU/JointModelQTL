0ef3412a0505c19589db34b07546669b07fa3700
Jiayu Su <jiayu_su@outlook.com>
2021-05-18 03:21:55 -0400

    Fix a bug which tried to compile non-stan files

--- R/load_models.R
+++ R/load_models.R
@@ -1,7 +1,6 @@
 load_models <- function() {
-  MODELS_HOME <- "src"
   stan_dirs <- dir(
-    file.path(MODELS_HOME, "stan_models"),
+    file.path("src", "stan_models"),
     full.names = TRUE,
     recursive = FALSE
   )
@@ -9,12 +8,13 @@ load_models <- function() {
   stan_models <- lapply(stan_dirs, function(d) {
     cat(paste("Loading stan models in", d, "\n"))
 
-    stan_files <- dir(d, full.names = TRUE)
+    stan_files <- dir(d, full.names = TRUE, pattern = "\\.stan$")
     model_names <- sub("\\.stan$", "", basename(stan_files))
 
     models <- lapply(stan_files, function(f) {
       rstan::stan_model(file = f, obfuscate_model_name = FALSE)
     })
+
     names(models) <- model_names
 
     return(models)
